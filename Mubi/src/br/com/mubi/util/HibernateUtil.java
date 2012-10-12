5090d893475d725f923241a575e4e0304f406765
yujis2jeh <du.yujixd@gmail.com>
2012-10-12 18:55:41 -0300

    xd

--- Mubi/src/br/com/mubi/util/HibernateUtil.java
+++ Mubi/src/br/com/mubi/util/HibernateUtil.java
@@ -3,6 +3,8 @@ package br.com.mubi.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
+import br.com.mubi.usuario.Usuario;
+
 @SuppressWarnings("deprecation")
 public class HibernateUtil {
 	
@@ -11,6 +13,7 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		AnnotationConfiguration cfg = new AnnotationConfiguration();
 		cfg.configure();
+		cfg.addAnnotatedClass(Usuario.class);
 		return cfg.buildSessionFactory();
 	}
 	
