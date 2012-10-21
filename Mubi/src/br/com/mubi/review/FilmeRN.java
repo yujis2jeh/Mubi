b3280c6e9537cb273996b229c0e62c35d05d6183
Yujis2Jeh <Yujis2Jeh@Dui-PC>
2012-10-21 21:15:38 -0200

    Signed-off-by: Yujis2Jeh <Yujis2Jeh@Dui-PC>

--- Mubi/src/br/com/mubi/review/FilmeRN.java
+++ Mubi/src/br/com/mubi/review/FilmeRN.java
@@ -1,8 +1,40 @@
 package br.com.mubi.review;
 
 public class FilmeRN {
+	private Filme filme;
+	private boolean continuacao;
 	
-	public void continuarInclusao(Filme filme){
-	
+	public FilmeRN() {
+		setFilme(new Filme());
 	}
+	
+	public boolean continuarInclusao(){
+		if(filme.getAnoLancamento() == 0 || filme.getClassificacao().isEmpty() || filme.getDirecao().isEmpty() || filme.getDuracao() == 0 || filme.getGenero().isEmpty() || filme.getProdutora().isEmpty()
+				|| filme.getTituloBrasil().isEmpty() || filme.getTituloOriginal().isEmpty()){
+			return continuacao;
+		} else {
+			return continuacao = true;
+		}
+	}
+	
+	public void voltar(){
+		continuacao = false;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+	public boolean isContinuacao() {
+		return continuacao;
+	}
+
+	public void setContinuacao(boolean continuacao) {
+		this.continuacao = continuacao;
+	}
+
 }
